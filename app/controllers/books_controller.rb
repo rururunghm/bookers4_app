9c95d956d68af6f1473dc6148ff0d6a19effcdc4
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2024-01-02 08:09:57 +0000

    update/rooting

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,31 @@
+class BooksController < ApplicationController
+  
+  def index
+    
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+end
